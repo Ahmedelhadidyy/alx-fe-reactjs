ff272236125073681f4428ba976636e25c2b1586
ahmed elhadidy <ahmedtamerelhadidy1@gmail.com>
2024-08-17 18:01:38 +0300

    first commit

--- my-company/src/App.jsx
+++ my-company/src/App.jsx
@@ -1,11 +1,21 @@
-
-import './App.css'
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import About from './components/About/About'
+import Services from './components/Services/Services'
+import Contact from './components/Contact/Contact'
+import Home from './components/Home/Home'
+import Navbar from './Navbar';
 
 function App() {
 
   return (
     <>
+      <Navbar />
       <h1>Hello World</h1>
+      <Services />
+      <Contact />
+      <Home />
+      <About />
     </>
   )
 }
--- my-company/src/components/Services/Services.jsx
+++ my-company/src/components/Services/Services.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-const Services = () => {
-  return (
-    <div>Services</div>
-  )
-}
+function Services() {
+    return (
+      <div style={{ padding: '20px' }}>
+        <h1>Our Services</h1>
+        <ul>
+          <li>Technology Consulting</li>
+          <li>Market Analysis</li>
+          <li>Product Development</li>
+        </ul>
+      </div>
+    );
+  }
 
-export default Services
+  export default Services;
