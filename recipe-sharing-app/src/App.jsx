c2967aec249875c8b28371153200784faf98ce9d
ahmed elhadidy <ahmedtamerelhadidy1@gmail.com>
2024-08-23 06:56:07 +0300

    first commit

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,15 +1,17 @@
-import RecipeList from "./components/RecipeList"
-import AddRecipeForm from "./components/AddRecipeForm"
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import RecipeList from './components/RecipeList';
+import RecipeDetails from './components/RecipeDetails';
 
-
-function App() {
-
+const App = () => {
   return (
-    <>
-      <RecipeList/>
-      <AddRecipeForm/>
-    </>
-  )
-}
+    <Router>
+      <Routes>
+        <Route path="/"  element={<RecipeList/>} />
+        <Route path="/recipe" element={<RecipeDetails />} />
+      </Routes>
+    </Router>
+    
+  );
+};
 
-export default App
+export default App;
