5abe12ae8717e9339d1fe9213fd7aa1ef5f2f0c3
ahmed elhadidy <ahmedtamerelhadidy1@gmail.com>
2024-08-30 22:05:23 +0300

    first commit

--- react-query-demo/src/App.jsx
+++ react-query-demo/src/App.jsx
@@ -1,5 +1,5 @@
 import { QueryClient, QueryClientProvider } from 'react-query';
-import PostsComponent from './component/PostsComponent';
+import PostsComponent from './Components/PostsComponent';
 
 const queryClient = new QueryClient();
 
