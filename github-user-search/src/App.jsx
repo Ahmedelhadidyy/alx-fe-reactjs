2930d368e24478e2594cfad0953ab49d236ae7b6
ahmed elhadidy <ahmedtamerelhadidy1@gmail.com>
2024-09-18 04:56:42 +0300

    Initial setup for GitHub User Search Application

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -1,12 +1,19 @@
 
+import './App.css';
+
+// Components (you will create these files later)
+import SearchForm from './components/SearchForm';
+import UserProfile from './components/UserProfile';
 
 function App() {
-  return (
-    <div className="App">
-      <h1>GitHub User Search Application</h1>
-      
-    </div>
-  );
+    return (
+        <div className="App">
+            <h1>GitHub User Search</h1>
+            <SearchForm />
+            <UserProfile />
+        </div>
+    );
 }
 
 export default App;
+
