e4d927a6d408d945f38632b7d050ed93241b30ba
ahmed elhadidy <ahmedtamerelhadidy1@gmail.com>
2024-09-18 05:59:22 +0300

    Initial setup for GitHub User Search Application

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -6,18 +6,3 @@ export const fetchUserData = async (username) => {
     const response = await axios.get(`${GITHUB_API_URL}/${username}`);
     return response.data;
 };
-
-
-export async function fetchAdvancedUserData({ username, location, minRepos }) {
-    try {
-        let query = "";
-        if (username) query += `user:${username}`;
-        if (location) query += ` location:${location}`;
-        if (minRepos) query += ` repos:>=${minRepos}`;
-
-        const response = await axios.get(`https://api.github.com/search/users?q=${query}`);
-        return response.data.items; // This returns an array of user objects
-    } catch (error) {
-        throw new Error("Error fetching users");
-    }
-}
